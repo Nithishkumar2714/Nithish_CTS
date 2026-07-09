7f341f48cce4f47c1bbde832e7c3f1d9badd7619
Nithishkumar2714 <nitkum275@gmail.com>
2026-07-09 16:39:54 +0530

    Add files via upload

---
+++ Spring rest 2/rest-country/src/main/java/com/cognizant/rest_country/controller/CountryController.java
@@ -0,0 +1,18 @@
+package com.cognizant.rest_country.controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cognizant.rest_country.model.Country;
+import com.cognizant.rest_country.service.CountryService;
+
+@RestController
+public class CountryController {
+    @Autowired
+    private CountryService countryService;
+    @GetMapping("/countries/{code}")
+    public Country getCountry(@PathVariable String code) {
+        return countryService.getCountry(code);
+    }
+}
